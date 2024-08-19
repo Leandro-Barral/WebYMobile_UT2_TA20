6eb63d6edfdaa9ab09f0d2d7a2597b67e727b4e9
Leandro Barral <leandro.barral@correo.ucu.edu.uy>
2024-08-19 12:03:58 -0300

    Cambio de texto alternativo al pasar mouse

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const imagen = document.getElementById("imagen");
 
 imagen.addEventListener("mouseover", () => {
     imagen.src = "imagen-alternativa.jpg";
+    imagen.alt = "Imagen Alternativa";
 });
 
 imagen.addEventListener("mouseout", () => {
     imagen.src = "imagen-original.jpg";
+    imagen.alt = "Imagen Original";
 });
